d7dd9f872fc36ae59b26cdbd1d394272d0741ff1
utca216 <utcano@gmail.com>
2024-10-28 21:23:14 +0600

    commit 1

--- src/main/java/com/example/electronicdevices/HelloController.java
+++ src/main/java/com/example/electronicdevices/HelloController.java
@@ -2,10 +2,17 @@ package com.example.electronicdevices;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField TextField4;
+    @FXML
+    private TextField TextField5;
+    @FXML
+    private
 
     @FXML
     protected void onHelloButtonClick() {
